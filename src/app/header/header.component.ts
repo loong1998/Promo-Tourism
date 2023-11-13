25bcec059ff686d0b59ee177944c5abc15cb9c86
loong1998 <loong19981@gmail.com>
2023-11-13 17:48:12 +0800

    update
    Update of login

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -40,38 +40,17 @@ export class HeaderComponent {
     }
   }
 
+  checkLogin(){
+    if(this.authService.getLoginUser() === null){
+      this.router.navigate(['/login']);
+    }
+  }
+
   logout() {
     // Perform logout actions, e.g., clearing session, token, etc.
     this.authService.logout();
 
     // Redirect to the login page after logout
     this.router.navigate(['/login']);
-  }
-
-  //To check if user is logged in
-  checkLoginP(){
-    //if isLoggedIn = false
-    if(!this.isLoggedIn){
-      //navigate to login page
-      this.router.navigate(['/login']);
-    }
-    //if isLoggedIn = true
-    else if(this.isLoggedIn){
-      //navigate to product page
-      this.router.navigate(['/product'])
-    }
-  }
-
-  checkLoginR(){
-    //if isLoggedIn = false
-    if(!this.isLoggedIn){
-      //navigate to login page
-      this.router.navigate(['/login']);
-    }
-    //if isLoggedIn = true
-    else if(this.isLoggedIn){
-      //navigate to review product page
-      this.router.navigate(['/reviewProduct'])
-    }
   }
 }
