5e9aac23ed8dd245bb25af500b27078219f10e41
loong1998 <loong19981@gmail.com>
2023-11-16 23:47:05 +0800

    updated reviewProduct.service

--- src/app/services/reviewProduct.service.ts
+++ src/app/services/reviewProduct.service.ts
@@ -1,6 +1,9 @@
 import {Injectable} from '@angular/core';
 import { ReviewProduct } from './reviewProduct.model';
 import { PaidProduct } from './reviewProduct.model';
+import { AuthService } from './auth.service';
+import { HttpClient } from '@angular/common/http';
+import { Subject } from 'rxjs';
 
 @Injectable(
     {
@@ -11,6 +14,9 @@ import { PaidProduct } from './reviewProduct.model';
 export class ReviewProductService{
     private reviewProducts: ReviewProduct[] = [];
     private paidProducts: PaidProduct[] = [];
+    private reviewUpdated = new Subject<ReviewProduct[]>();
+
+    constructor(public authService: AuthService, private http: HttpClient){}
 
     getPaidProducts(){
         return this.paidProducts;
@@ -27,7 +33,17 @@ export class ReviewProductService{
     }
 
     addReviewProduct(productID:string,  review: string){
-        const reviewProduct: ReviewProduct = {productID:productID, review:review};
-        this.reviewProducts.push(reviewProduct);
+        const newReview: ReviewProduct = {reviewID: "", productID:productID, review:review};
+
+        this.http.post<{message: string, reviewID: string}>('http://localhost:3000/api/review', newReview)
+            .subscribe(
+                (response) => {
+                    console.log(response.message);
+                    const reviewID = response.reviewID;
+                    newReview.reviewID=reviewID;
+                    this.reviewProducts.push(newReview);
+                    this.reviewUpdated.next([...this.reviewProducts]);
+                }
+            )
     }
 }
